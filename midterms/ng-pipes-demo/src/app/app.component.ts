7cc13dcb49b8c01d2d39e1f7b8438ff307cf9ac0
SJH-706 <sjh-706@SJH-706-33.local>
2025-01-15 10:10:49 +0800

    midterms pipes

---
+++ midterms/ng-pipes-demo/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { interval } from 'rxjs';
+import { map } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    CommonModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+
+export class AppComponent {
+  title = 'ng-pipes-demo';
+  presentDate = new Date(); 
+
+  // AsyncPipe
+  time$ = interval(1000).pipe(
+    map(() => new Date())
+  )
+
+  // JsonPipe
+  testObject = {
+    name: 'Bryan Aaron Santiago',
+    age: 20,
+    food: 'Cookies'
+  }
+
+  // Currency Pipe
+  price: number = 20000;
+
+  // Slice Pipe
+  Fruits = ["Apple", "Orange", "Grapes", "Mango", "Kiwi", "Pomegranate"];
+
+  // Decimal Pipe
+  decimalNum1: number = 8.2343242;
+  decimalNum2: number = 5.43;
+
+}
