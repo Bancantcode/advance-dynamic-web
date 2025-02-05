cef13ded87ffa9356a5f006876946b68ecd99fb0
Bancantcode <santiagobryan831@gmail.com>
2025-02-05 11:04:59 +0800

    fixed app.component.ts

--- midterms/ng-pipes-demo/src/app/app.component.ts
+++ midterms/ng-pipes-demo/src/app/app.component.ts
@@ -20,38 +20,38 @@ import { NewCmpComponent } from './new-cmp/new-cmp.component';
 
 export class AppComponent {
   title = 'ng-pipes-demo';
-  currentDate = new Date('2024-04-26T15:30:45'); 
+  // presentDate = new Date(); 
 
-  // AsyncPipe
-  time$ = interval(1000).pipe(
-    map(() => new Date())
-  )
+  // // AsyncPipe
+  // time$ = interval(1000).pipe(
+  //   map(() => new Date())
+  // )
 
-  // JsonPipe
-  testObject = {
-    name: 'Bryan Aaron Santiago',
-    age: 20,
-    food: 'Cookies'
-  }
+  // // JsonPipe
+  // testObject = {
+  //   name: 'Bryan Aaron Santiago',
+  //   age: 20,
+  //   food: 'Cookies'
+  // }
 
-  // Currency Pipe
-  price: number = 20000;
+  // // Currency Pipe
+  // price: number = 20000;
 
-  // Slice Pipe
-  Fruits = ["Apple", "Orange", "Grapes", "Mango", "Kiwi", "Pomegranate"];
+  // // Slice Pipe
+  // Fruits = ["Apple", "Orange", "Grapes", "Mango", "Kiwi", "Pomegranate"];
 
-  // Decimal Pipe
-  decimalNum1: number = 8.2343242;
-  decimalNum2: number = 5.43;
+  // // Decimal Pipe
+  // decimalNum1: number = 8.2343242;
+  // decimalNum2: number = 5.43;
 
-  todaydate;
-  constructor(private myservice:MyServiceService) {
-    this.todaydate = this.myservice.showTodayDate();
-  }
+  // todaydate;
+  // constructor(private myservice:MyServiceService) {
+  //   this.todaydate = this.myservice.showTodayDate();
+  // }
 
   // new
-  // currentDate = new Date('2024-04-26T15:30:45');
-  // price = 42.42424242;
+  currentDate = new Date('2024-04-26T15:30:45');
+  price = 42.42424242;
 
     arrayStream$ = of([1,2,3,4,5]).pipe(delay(1000));
     transformedData$ = of({ key: 'value', num: 42}).pipe(
